e174113b6643e79b00c4674b5dc36ef7ba4fb80f
Evgen Filatov <evgen.filatov@gmail.com>
2019-01-09 22:02:15 +0200

    Added cube

--- src/index.ts
+++ src/index.ts
@@ -98,29 +98,75 @@ async function createProgram(gl: WebGLRenderingContext): Promise<Program | null>
 
 function createBuffers(gl: WebGLRenderingContext) {
     const positions = [
-         1.0,  1.0,
-        -1.0,  1.0,
-         1.0, -1.0,
-        -1.0, -1.0,
+        // Front face
+        -1.0, -1.0,  1.0,
+         1.0, -1.0,  1.0,
+         1.0,  1.0,  1.0,
+        -1.0,  1.0,  1.0,
+        // Back face
+        -1.0, -1.0, -1.0,
+        -1.0,  1.0, -1.0,
+         1.0,  1.0, -1.0,
+         1.0, -1.0, -1.0,
+        // Top face
+        -1.0,  1.0, -1.0,
+        -1.0,  1.0,  1.0,
+         1.0,  1.0,  1.0,
+         1.0,  1.0, -1.0,
+        // Bottom face
+        -1.0, -1.0, -1.0,
+         1.0, -1.0, -1.0,
+         1.0, -1.0,  1.0,
+        -1.0, -1.0,  1.0,
+        // Right face
+         1.0, -1.0, -1.0,
+         1.0,  1.0, -1.0,
+         1.0,  1.0,  1.0,
+         1.0, -1.0,  1.0,
+        // Left face
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0,  1.0,
+        -1.0,  1.0,  1.0,
+        -1.0,  1.0, -1.0,
     ];
-    const colors = [
-        1.0,  1.0,  1.0,  1.0,
-        1.0,  0.0,  0.0,  1.0,
-        0.0,  1.0,  0.0,  1.0,
-        0.0,  0.0,  1.0,  1.0,
+    const faceColors = [
+        [1.0, 1.0, 1.0, 1.0],    // Front face: white
+        [1.0, 0.0, 0.0, 1.0],    // Back face: red
+        [0.0, 1.0, 0.0, 1.0],    // Top face: green
+        [0.0, 0.0, 1.0, 1.0],    // Bottom face: blue
+        [1.0, 1.0, 0.0, 1.0],    // Right face: yellow
+        [1.0, 0.0, 1.0, 1.0],    // Left face: purple
     ];
+
+    let colors: number[] = [];
+
+    for (var j = 0; j < faceColors.length; ++j) {
+        const c = faceColors[j];
+        colors = colors.concat(c, c, c, c);
+    }
+
+    const indices = [
+        0, 1, 2, 0, 2, 3,    // front
+        4, 5, 6, 4, 6, 7,    // back
+        8, 9, 10, 8, 10, 11,   // top
+        12, 13, 14, 12, 14, 15,   // bottom
+        16, 17, 18, 16, 18, 19,   // right
+        20, 21, 22, 20, 22, 23,   // left
+    ];
+    
     return {
-        position: createBuffer(gl, positions),
-        color: createBuffer(gl, colors)
+        position: createBuffer(gl, gl.ARRAY_BUFFER, new Float32Array(positions)),
+        color: createBuffer(gl, gl.ARRAY_BUFFER, new Float32Array(colors)),
+        indices: createBuffer(gl, gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices))
     }
 }
 
-function createBuffer(gl: WebGLRenderingContext, data: number[]) {
+function createBuffer(gl: WebGLRenderingContext, type: number, data: Float32Array | Uint16Array) {
     const buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bindBuffer(type, buffer);
     gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array(data),
+        type,
+        data,
         gl.STATIC_DRAW
     );
     return buffer as WebGLBuffer;
@@ -154,13 +200,15 @@ function drawScene(program: Program, buffers: {[key: string]: WebGLBuffer}, dt: 
 
     const fovy = 45 * Math.PI / 180;
     const ratio = width / height;
-    const rotation = dt / 10 / 180;
+    const rotation = dt / 7 / 180;
     Mat4.perspective(uPMatrix, fovy, ratio, 0.1, 100.0);
     Mat4.translate(uMVMatrix, uMVMatrix, [-0.0, 0.0, -6.0]);
-    Mat4.rotate(uMVMatrix, uMVMatrix, rotation, [0, 0, 1])
+    Mat4.rotate(uMVMatrix, uMVMatrix, rotation, [0, 1, 1])
 
-    sendBuffer(gl, buffers.position, program.attributes.aVertexPosition, 2);
+    sendBuffer(gl, buffers.position, program.attributes.aVertexPosition, 3);
     sendBuffer(gl, buffers.color, program.attributes.aVertexColor, 4);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
 
     gl.useProgram(program);
     gl.uniformMatrix4fv(
@@ -174,6 +222,6 @@ function drawScene(program: Program, buffers: {[key: string]: WebGLBuffer}, dt: 
         uMVMatrix
     );
 
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    gl.drawElements(gl.TRIANGLE_STRIP, 36, gl.UNSIGNED_SHORT, 0);
 
 }
