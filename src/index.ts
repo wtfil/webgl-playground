b0f75db15c5c3f971b8095b23f6fe0e82c36af62
Evgen Filatov <evgen.filatov@gmail.com>
2019-01-09 21:18:11 +0200

    Added colors

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,12 @@ async function setup() {
     document.body.appendChild(canvas);
 
     const program = await createProgram(gl);
-    const buffer = createBuffer(gl);
-    if (!buffer || !program) {
+    const buffers = createBuffers(gl);
+    if (!buffers || !program) {
         return;
     }
 
-    drawScene(program, buffer);
+    drawScene(program, buffers);
 }
 
 async function loadShader(url: string, gl: WebGLRenderingContext, type: number) {
@@ -47,6 +47,7 @@ export type Program = WebGLProgram & {
     },
     attributes: {
         aVertexPosition: number;
+        aVertexColor: number;
     },
     matrix: {
         uPMatrix: Mat4,
@@ -86,7 +87,8 @@ async function createProgram(gl: WebGLRenderingContext): Promise<Program | null>
         uPMatrix: getUniform('uPMatrix')
     };
     program.attributes = {
-        aVertexPosition: gl.getAttribLocation(program, 'aVertexPosition')
+        aVertexPosition: gl.getAttribLocation(program, 'aVertexPosition'),
+        aVertexColor: gl.getAttribLocation(program, 'aVertexColor')
     };
     program.matrix = {uPMatrix, uMVMatrix}
     program.gl = gl;
@@ -94,24 +96,50 @@ async function createProgram(gl: WebGLRenderingContext): Promise<Program | null>
     return program;
 }
 
-function createBuffer(gl: WebGLRenderingContext) {
-    const buffer = gl.createBuffer();
+function createBuffers(gl: WebGLRenderingContext) {
     const positions = [
          1.0,  1.0,
         -1.0,  1.0,
          1.0, -1.0,
         -1.0, -1.0,
     ];
+    const colors = [
+        1.0,  1.0,  1.0,  1.0,
+        1.0,  0.0,  0.0,  1.0,
+        0.0,  1.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,  1.0,
+    ];
+    return {
+        position: createBuffer(gl, positions),
+        color: createBuffer(gl, colors)
+    }
+}
+
+function createBuffer(gl: WebGLRenderingContext, data: number[]) {
+    const buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.bufferData(
         gl.ARRAY_BUFFER,
-        new Float32Array(positions),
+        new Float32Array(data),
         gl.STATIC_DRAW
     );
-    return buffer;
+    return buffer as WebGLBuffer;
 }
 
-function drawScene(program: Program, buffer: WebGLBuffer) {
+function sendBuffer(gl: WebGLRenderingContext, buffer: WebGLBuffer, attribute: number, numComponents: number) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.vertexAttribPointer(
+        attribute,
+        numComponents,
+        gl.FLOAT,
+        false,
+        0,
+        0
+    );
+    gl.enableVertexAttribArray(attribute);
+}
+
+function drawScene(program: Program, buffers: {[key: string]: WebGLBuffer}) {
     const {gl, matrix: {uPMatrix, uMVMatrix}} = program;
     const width = gl.canvas.clientWidth;
     const height = gl.canvas.clientHeight;
@@ -127,16 +155,8 @@ function drawScene(program: Program, buffer: WebGLBuffer) {
     Mat4.perspective(uPMatrix, fovy, ratio, 0.1, 100.0);
     Mat4.translate(uMVMatrix, uMVMatrix, [-0.0, 0.0, -6.0]);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.vertexAttribPointer(
-        program.attributes.aVertexPosition,
-        2,
-        gl.FLOAT,
-        false,
-        0,
-        0
-    );
-    gl.enableVertexAttribArray(program.attributes.aVertexPosition);
+    sendBuffer(gl, buffers.position, program.attributes.aVertexPosition, 2);
+    sendBuffer(gl, buffers.color, program.attributes.aVertexColor, 4);
 
     gl.useProgram(program);
     gl.uniformMatrix4fv(
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,9 +1,13 @@
 attribute vec4 aVertexPosition;
+attribute vec4 aVertexColor;
 
 uniform mat4 uMVMatrix;
 uniform mat4 uPMatrix;
 
+varying lowp vec4 vColor;
+
 void main() {
   gl_Position = uPMatrix * uMVMatrix * aVertexPosition;
+  vColor = aVertexColor;
 }
 
