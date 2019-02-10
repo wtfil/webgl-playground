7fa12f78f9ab641f1c807c379856b36855c69e77
Evgen Filatov <evgen.filatov@gmail.com>
2019-02-10 22:48:10 +0200

    less toxic water

--- src/shaders/water.fragment.glsl
+++ src/shaders/water.fragment.glsl
@@ -4,10 +4,8 @@ uniform sampler2D texture;
 uniform lowp float dudvOffset;
 
 void main() {
-  //gl_FragColor = texture2D(texture, vTextureCoord);
-  // gl_FragColor = texture2D(texture, vec2(vTextureCoord.x + dudvOffset, vTextureCoord.y));
   lowp vec2 distortedTexCoords = texture2D(texture, vec2(vTextureCoord.x + dudvOffset, vTextureCoord.y)).rg * 0.1;
   distortedTexCoords = vTextureCoord + vec2(distortedTexCoords.x, distortedTexCoords.y + dudvOffset);
 
-  gl_FragColor = texture2D(texture, distortedTexCoords);
+  gl_FragColor = vec4(0, 0, distortedTexCoords * vec2(1, 1));
 }
